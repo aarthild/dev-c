e19fa471e9e6b67452f380124a5790e7c471ed8d
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-03 22:09:31 +0530

    Add files via upload

---
+++ arraycall.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void print_array(int a[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	print_array(a,n);
+}
