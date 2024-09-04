03d8d40e771f6b22c698a6b5901c733fb3f8135f
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-04 19:29:28 +0530

    Add files via upload

---
+++ bwop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=2;
+	int res=a&b;
+	printf("a&b=%d\n",res);
+	res=a|b;
+	printf("a|b=%d\n",res);
+	res=a^b;
+	printf("a^b=%d\n",res);
+	res=a<<b;
+	printf("a<<b=%d",res);
+	
+}
