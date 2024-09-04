af3b58570226db37a421e00ebd55375416656981
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-04 19:28:44 +0530

    Add files via upload

---
+++ beforeprime.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int prime(int n)
+{
+	int i,c=0;
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			c++;
+		}
+	}
+	if(c==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	for(i=n-1;;i--)
+	{
+		if(prime(i))
+		{
+			printf("%d",i);
+			break;
+		}
+	}
+}
+	
