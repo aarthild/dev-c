36eb842a455be414d7285572dd02cdf81ada6f71
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-08 16:25:22 +0530

    Add files via upload

---
+++ FOODCOST.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x,y;
+    cin>>x;
+    cin>>y;
+   int messcost=6*x;
+    int sundaycost=y;
+    cout <<messcost+sundaycost;
+}
