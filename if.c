501caa75d3b1e28a6bffb8cd57fa029e2cd3c7af
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-10 13:56:41 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d\n%d",&a,&b);
+	if(a>b)
+	{
+	
+	printf("a is greater than b\n");
+    }
+	if(a<b)
+	{
+		
+	printf("a is less than b");
+}
+}
