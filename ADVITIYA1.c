6f5607aced9f579b3e80936dea8ee9dc5964bad0
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-13 08:15:34 +0530

    Add files via upload

---
+++ ADVITIYA1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==16||n==17||n==18)
+	{
+	    printf("ADVITIYA");
+	}
+	else 
+	{
+	    printf("WAITING FOR ADVITIYA");
+	}
+
+}
+
