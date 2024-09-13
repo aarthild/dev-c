61dbd6f48f9156649af57013b9d2727bde765b8b
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-13 08:14:43 +0530

    Add files via upload

---
+++ HEALSE.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int x;
+scanf("%d",&x);
+if(x<8)
+{
+    printf("LESS");
+}
+else if(x==8)
+{
+    printf("PERFECT");
+}
+else
+{
+    printf("MORE");
+}
+}
+
