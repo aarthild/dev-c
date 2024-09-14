c7c6f7f047a01cb5ec3a23c1d420e91e1a21ff3f
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-14 13:22:46 +0530

    Add files via upload

---
+++ LUDO.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+ int x,t;
+ scanf("%d",&t);
+ while(t--)
+ {
+     scanf("%d",&x);
+     if(x==6)
+     {
+         printf("YES\n");
+     }
+     else
+     {
+         printf("NO\n");
+     }
+ }
+
+}
+
