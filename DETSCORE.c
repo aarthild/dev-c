4a8678fa9ed1ef807169232f21a667f9b7e6f1d3
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-15 15:54:58 +0530

    Add files via upload

---
+++ DETSCORE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int x,y,t,a;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    scanf("%d",&y);
+	    a=x/10;
+	    printf("%d\n",a*y);
+	    
+	}
+
+}
+
