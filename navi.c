0f5a64197300b299dba9566b091e3969c1dbb681
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-16 11:48:52 +0530

    Add files via upload

---
+++ navi.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	int a,b;
+	scanf("%c",&ch);
+	scanf("%d %d",&a,&b);
+	switch(ch)
+	{
+		case '+':
+			printf("%d",a+b);
+			break;
+			case '-':
+			printf("%d",a-b);
+			break;
+			case '*':
+			printf("%d",a*b);
+			break;
+			case '/':
+			printf("%d",a/b);
+			break;
+			default :
+				printf("invalid operator");
+			
+	}
+}
