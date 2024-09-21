73cdf648f07d3766ee2a37e571d2b434064b6f11
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-21 08:47:22 +0530

    Add files via upload

---
+++ KITCHENTIME.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x,t,y;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    scanf("%d",&y);
+	    printf("%d\n",y-x);
+	}
+
+}
+
