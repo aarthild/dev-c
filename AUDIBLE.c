440b41bcda50343ed2f9ea31e981bb8daaf9b5a4
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-21 08:46:33 +0530

    Add files via upload

---
+++ AUDIBLE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,n,m;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&n);
+	    if(n>=67 && n<=45000)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	    
+	}
+	
+
+}
+
