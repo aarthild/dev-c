d95cf34af7ea6db12f0665fc9077472b87cf7fc8
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-21 08:46:21 +0530

    Add files via upload

---
+++ TIMELY.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    if(x>=30)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}
+
+}
+
