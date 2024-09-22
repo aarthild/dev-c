3890aa70ac117c1206ffdd8eb72d38919320db27
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-22 20:05:41 +0530

    Add files via upload

---
+++ pcnpccount.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int prime(int n)
+{
+	int i,c=0;
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			c++;
+		}
+	}
+	if(c==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+void prime_non_prime_count(int a[],int n)
+{
+	int i,pc=0,npc=0;
+	for(i=0;i<n;i++)
+	{
+		if(prime(a[i]))
+		{
+			pc++;
+		}
+		else
+		{
+			npc++;
+		}
+	}
+	printf("%d %d",pc,npc);
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	prime_non_prime_count(a,n);
+	
+}
