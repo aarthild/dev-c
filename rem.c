ab0d6373c486d517ad670ac231a1ac4d5fc80424
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-23 13:41:00 +0530

    Add files via upload

---
+++ rem.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20,rem;
+	rem=(a>b);
+	printf("(a>b)=%d\n",rem);
+	rem=(a<b);
+	printf("(a<b)=%d\n",rem);
+	rem=(a<=b);
+	printf("(a<=b)=%d\n",rem);
+	rem=(a>=b);
+	printf("(a>=b)=%d\n",rem);
+	
+}
