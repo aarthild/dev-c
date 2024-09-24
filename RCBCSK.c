43aeedcfecee6713e7940798f44ce86783577d64
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-24 16:56:15 +0530

    Add files via upload

---
+++ RCBCSK.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x,y;
+	scanf("%d%d",&x,&y);
+	if((x-y)>=18)
+	{
+	    printf("RCB");
+	}
+	else
+	{
+	    printf("CSK");
+	}
+
+}
+
