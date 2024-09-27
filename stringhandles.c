3b1391db7b1d8dfa989b1cdf8c80fa62df3a6ca9
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-27 21:22:10 +0530

    Add files via upload

---
+++ stringhandles.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[]="AARTHI",ing[]="ANANTHA";
+//	printf("%d\n",strlen(str));
+//	printf("%d\n",sizeof(str));
+//	printf("%s\n",strupr(str));
+//	printf("%s\n",strlwr(str));
+//	printf("%s\n",strrev(str));
+//	printf("%s\n",strcpy(str,ing));
+//	printf("%s\n",strcat(str,ing));
+printf("%s\n",(strcmp(str,ing)));
+
+}
