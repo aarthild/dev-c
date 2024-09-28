c678863b2a855f46352a66769045b3780c5536a4
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-09-28 20:55:33 +0530

    Add files via upload

---
+++ stringsum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100];
+	gets(str);
+	int i,sum=0;
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>'0' && str[i]<='9')
+		{
+			sum=sum+str[i]-'0';
+		}
+	}
+	printf("%d",sum);
+	
+}
