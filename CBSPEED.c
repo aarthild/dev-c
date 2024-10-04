9aeb045f0113c188d4a9100bd6970c7d4ef71d9a
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-04 08:39:52 +0530

    Add files via upload

---
+++ CBSPEED.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    // Your code goes here (if needed)
+    if(y>x)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+}
