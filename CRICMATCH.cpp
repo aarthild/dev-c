70e455cbb064558c1cf1f2d13d49bf61d811e37c
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-16 17:58:05 +0530

    Add files via upload

---
+++ CRICMATCH.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	for(int i=0;i<x;i++)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if(b*6*6>=a)
+	    {
+	        cout<<"yes\n";
+	    }
+	    else
+	    {
+	        cout<<"No\n";
+	    }
+	}
+
+}
