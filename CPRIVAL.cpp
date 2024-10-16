594397ba781939d3e274f3e682c7d44f7338d949
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-16 17:58:33 +0530

    Add files via upload

---
+++ CPRIVAL.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	double r1,r2,d1,d2;
+	cin>>r1>>r2;
+	cin>>d1>>d2;
+	if(r1+d1>r2+d2)
+	{
+	    cout<<"Dominater";
+	}
+	else
+	{
+	    cout<<"Everule";
+	}
+	
+
+}
