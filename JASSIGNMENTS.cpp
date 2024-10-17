c5de58874eda87dbf8125208f16fc4a2aa6efbd0
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-17 19:29:37 +0530

    Add files via upload

---
+++ JASSIGNMENTS.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	while(x--)
+	{
+	    int a;
+	    cin>>a;
+	    if(a+3<=10)
+	    {
+	        cout<<"Yes\n";
+	    }
+	    else
+	    {
+	        cout<<"no\n";
+	    }
+	}
+
+}
