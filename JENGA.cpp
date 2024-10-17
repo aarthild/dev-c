5c5608988ff92eba536661ec9eb9f833add61931
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-17 19:29:22 +0530

    Add files via upload

---
+++ JENGA.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	while(x--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if(b%a==0)
+	    {
+	        cout<<"yes\n";
+	    }
+	    else
+	    {
+	        cout<<"no\n";
+	    }
+	}
+
+}
