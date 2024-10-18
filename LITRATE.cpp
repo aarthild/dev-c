3639eac1b255172c1359f4808f88b605b781efe8
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-18 11:35:34 +0530

    Add files via upload

---
+++ LITRATE.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	while(x--)
+	{
+	    float a,b;
+	    cin>>a>>b;
+	    if((b/a)>=0.75)
+	    {
+	        cout<<"Yes\n";
+	    }
+	    else
+	    {
+	        cout<<"no\n";
+	    }
+	}
+
+}
