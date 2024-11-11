873d698541b9fa1fb52f090efc595e0c9012ff11
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-11-11 19:36:52 +0530

    Add files via upload

---
+++ DIFFSUM.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a>b)
+	{
+	    cout<<a-b;
+	}
+	else
+	{
+	    cout<<a+b;
+	}
+
+}
